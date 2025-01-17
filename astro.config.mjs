582350006339da3399376880dc2037d62d9a2110
jfzara <zarajeanfabrice@gmail.com>
2025-01-16 22:05:33 -0500

    fix 2

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,10 +5,5 @@ import cloudflare from '@astrojs/cloudflare';
 export default defineConfig({
   integrations: [tailwind()],
   output: 'server',
-  vite: {
-    envPrefix: 'RESEND_'
-  },
-  adapter: cloudflare({
-    mode: 'directory'
-  })
+  adapter: cloudflare()
 });
